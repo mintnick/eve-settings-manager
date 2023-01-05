fcf576cc392914a20c6e3e4cd0272e3cd441ac67
Ke Ning <mintnick@gmail.com>
2023-01-05 20:25:38 +0800

    finish UI

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-const createWindow = () => {
+function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
+
+  win.webContents.openDevTools()
 
   win.loadFile('./src/views/index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
