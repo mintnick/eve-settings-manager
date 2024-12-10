d4ad6c6a7aac5768d27fca249689d014f457f190
Nick Ning <mintnick@gmail.com>
2024-12-10 14:09:15 +0800

    add: new logo

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,18 @@ import './demos/ipc'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 createApp(App)
+  .use(vuetify)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
